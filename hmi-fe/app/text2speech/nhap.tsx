49f7f2dc2122bfb88db50e2d3d6ae1ed5f4840de
okusamaOwO <lanhuong2k4UwU@gmail.com>
2024-11-30 17:54:25 +0700

    ...len

--- hmi-fe/app/text2speech/nhap.tsx
+++ hmi-fe/app/text2speech/nhap.tsx
@@ -1,22 +1,21 @@
 import * as PlayHT from 'playht';
 import fs from 'fs';
-import player from 'play-sound'; // For playing audio
 
 const user_id = '31Ivv5ymNXOEmPxBSBzonXQr5PF2';
 const secret_key = '761a57aab3c84b1f96d30125f2fdec86';
 
-const audioPlayer = player(); // Initialize audio player
-
 async function generateAndStreamAudio() {
     try {
-        // Initialize client
+        
         PlayHT.init({
             userId: user_id,
             apiKey: secret_key,
         });
 
-        let input_text = "are you serious?";
-        const filePath = './public/audio/hello-playht.mp3';
+        let input_text = "Yes, integrating React with Django Admin is viable, but the process can be complex depending on the level of integration you want. Here’s an overview of how you can approach it:?";
+        // const filePath = "D:\\Downloads\\Kì này\\HMI\\-INT2041_55-Seendyrella\\hmi-fe\\public\\audio\\hello-playht.mp3";
+        const path = require("path");
+        const filePath = path.join(__dirname, "audio.mp3");
         const fileStream = fs.createWriteStream(filePath);
 
         const stream = await PlayHT.stream(input_text, {
@@ -25,13 +24,10 @@ async function generateAndStreamAudio() {
 
         stream.pipe(fileStream);
 
-        fileStream.on('finish', () => {
+        fileStream.on('finish', async () => {
             console.log('Audio file has been generated successfully');
-            
-            // Play the audio file
-            audioPlayer.play(filePath, (err) => {
-                if (err) console.error('Error playing audio:', err);
-            });
+            const sound = require('sound-play');
+            sound.play(filePath);
         });
 
         fileStream.on('error', (error) => {
